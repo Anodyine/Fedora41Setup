a0ee2b1901ccff287ea24f8289a77c0f626482be
anodyine <anodyine@2603-9001-0200-74e5-0000-0000-0000-0ca5.inf6.spectrum.com>
2024-11-12 05:56:36 -0500

    added -y

--- build-4.sh
+++ build-4.sh
@@ -1,20 +1,20 @@
-sudo dnf install vim-enhanced
+sudo dnf install -y vim-enhanced
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 dnf check-update
-sudo dnf install code
-sudo dnf install dnf-plugins-core
+sudo dnf install -y code
+sudo dnf install -y dnf-plugins-core
 curl -O https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
 sudo sed -i -e 's/autorefresh=1$//g' brave-browser.repo 
 sudo dnf config-manager addrepo --from-repofile=brave-browser.repo
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-sudo dnf install brave-browser
+sudo dnf install -y brave-browser
 sudo grub2-mkconfig -o /boot/grub2/grub.cfg
-sudo dnf install git
+sudo dnf install -y git
+sudo dnf install -y gh
 curl -L -o install-tl-unx.tar.gz https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
 zcat < install-tl-unx.tar.gz | tar xf - 
-cd install-tl-20241111/
-perl ./install-tl --no-interaction
+cd install-tl-20241112/
 sudo perl ./install-tl --no-interaction
 sudo dnf install perl-File-Copy
 sudo dnf install perl-Time-HiRes
